d2d235ac11e005d0725b738344595667121fc814
Michael Jolley <mike@sparcapp.io>
2021-04-28 14:57:05 -0500

    Changing image size

--- backend/imageContainer/src/screenshot.ts
+++ backend/imageContainer/src/screenshot.ts
@@ -4,7 +4,7 @@ export const screenshot = async (themeId: string): Promise<boolean> => {
 
   const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
   const page = await browser.newPage();
-  await page.setViewport({ width: 1920, height: 1080 });
+  await page.setViewport({ width: 1024, height: 768 });
   await page.goto('http://127.0.0.1:8080/');
 
   await page.waitForSelector('.activitybar');
